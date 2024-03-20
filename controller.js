288df5a59fcf1456e98788a5a48d902adb132305
ghufron akbar <alan.040219.gam@gmail.com>
2024-03-20 19:32:31 +0700

    routes api id

--- controller.js
+++ controller.js
@@ -21,6 +21,20 @@ exports.berita = function(req,res){
     )
 };
 
+//BERITA ID
+exports.beritaid = function(req,res){
+    let id = req.params.id;
+    connection.query('SELECT * FROM berita WHERE berita_id = ?', [id],
+    function(error,rows, field){
+        if(error){
+            connection.log(error);
+        }else{
+            response.ok(rows,res);
+        };
+    }
+    )
+};
+
 //CALON KETUA
 exports.calonketua = function(req,res){
     connection.query('SELECT * FROM calon_ketua', function(error, rows, fields){
@@ -28,6 +42,20 @@ exports.calonketua = function(req,res){
             connection.log(error);            
         }else{
             response.ok(rows, res)
+        };
+    }
+    )
+};
+
+//CALON KETUA ID
+exports.calonketuaid = function(req,res){
+    let id = req.params.id;
+    connection.query('SELECT * FROM calon_ketua WHERE calon_ketua_id = ?', [id],
+    function(error,rows, field){
+        if(error){
+            connection.log(error);
+        }else{
+            response.ok(rows,res);
         };
     }
     )
@@ -45,6 +73,7 @@ exports.informasidesa = function(req,res){
     )
 };
 
+
 //JENIS UMKM
 exports.jenisumkm = function(req,res){
     connection.query('SELECT * FROM jenis_umkm', function(error, rows, fields){
@@ -52,6 +81,20 @@ exports.jenisumkm = function(req,res){
             connection.log(error);            
         }else{
             response.ok(rows, res)
+        };
+    }
+    )
+};
+
+//JENIS UMKM ID
+exports.jenisumkmid = function(req,res){
+    let id = req.params.id;
+    connection.query('SELECT * FROM jenis_umkm WHERE jenis_umkm_id = ?', [id],
+    function(error,rows, field){
+        if(error){
+            connection.log(error);
+        }else{
+            response.ok(rows,res);
         };
     }
     )
@@ -69,9 +112,24 @@ exports.komentar = function(req,res){
     )
 };
 
-//CALON KETUA
-exports.calonketua = function(req,res){
-    connection.query('SELECT * FROM calon_ketua', function(error, rows, fields){
+//KOMENTAR ID
+exports.komentarid = function(req,res){
+    let id = req.params.id;
+    connection.query('SELECT * FROM komentar WHERE komentar_id = ?', [id],
+    function(error,rows, field){
+        if(error){
+            connection.log(error);
+        }else{
+            response.ok(rows,res);
+        };
+    }
+    )
+};
+
+
+//PEMILIHAN KETUA
+exports.pemilihanketua = function(req,res){
+    connection.query('SELECT * FROM pemilihan_ketua_id', function(error, rows, fields){
         if(error){
             connection.log(error);            
         }else{
@@ -81,13 +139,15 @@ exports.calonketua = function(req,res){
     )
 };
 
-//PEMILIHAN KETUA
-exports.pemilihanketua = function(req,res){
-    connection.query('SELECT * FROM pemilihan_ketua', function(error, rows, fields){
+//PEMILIHAN KETUA ID
+exports.pemilihanketuaid = function(req,res){
+    let id = req.params.id;
+    connection.query('SELECT * FROM pemilihan_ketua WHERE pemilihan_ketua_id = ?', [id],
+    function(error,rows, field){
         if(error){
-            connection.log(error);            
+            connection.log(error);
         }else{
-            response.ok(rows, res)
+            response.ok(rows,res);
         };
     }
     )
@@ -106,6 +166,21 @@ exports.pengaduanmasyarakat = function(req,res){
     )
 };
 
+
+//PENGADUAN MASYARKAT ID
+exports.pengaduanmasyarakatid = function(req,res){
+    let id = req.params.id;
+    connection.query('SELECT * FROM pengaduan_masyarakat WHERE pengaduan_masyarakat_id = ?', [id],
+    function(error,rows, field){
+        if(error){
+            connection.log(error);
+        }else{
+            response.ok(rows,res);
+        };
+    }
+    )
+};
+
 //PENGURUS DESA ANGGOTA
 exports.pengurusdesaanggota = function(req,res){
     connection.query('SELECT * FROM pengurus_desa_anggota', function(error, rows, fields){
@@ -113,6 +188,20 @@ exports.pengurusdesaanggota = function(req,res){
             connection.log(error);            
         }else{
             response.ok(rows, res)
+        };
+    }
+    )
+};
+
+//PENGURUS DESA ANGGOTA ID
+exports.pengurusdesaanggotaid = function(req,res){
+    let id = req.params.id;
+    connection.query('SELECT * FROM pengurus_desa_anggota WHERE pengurus_desa_anggota_id = ?', [id],
+    function(error,rows, field){
+        if(error){
+            connection.log(error);
+        }else{
+            response.ok(rows,res);
         };
     }
     )
@@ -130,13 +219,42 @@ exports.umkm = function(req,res){
     )
 };
 
-//CALON warga
+//UMKM ID
+exports.umkmid = function(req,res){
+    let id = req.params.id;
+    connection.query('SELECT * FROM umkm WHERE umkm_id = ?', [id],
+    function(error,rows, field){
+        if(error){
+            connection.log(error);
+        }else{
+            response.ok(rows,res);
+        };
+    }
+    )
+};
+
+
+//WARGA
 exports.warga = function(req,res){
     connection.query('SELECT * FROM warga', function(error, rows, fields){
         if(error){
             connection.log(error);            
         }else{
             response.ok(rows, res)
+        };
+    }
+    )
+};
+
+//WARGA ID
+exports.wargaid = function(req,res){
+    let id = req.params.id;
+    connection.query('SELECT * FROM warga WHERE warga_id = ?', [id],
+    function(error,rows, field){
+        if(error){
+            connection.log(error);
+        }else{
+            response.ok(rows,res);
         };
     }
     )
--- routes.js
+++ routes.js
@@ -10,8 +10,14 @@ module.exports = function(app)  {
     app.route('/berita')
     .get(jsonku.berita);
 
+    app.route('/berita/:id')
+    .get (jsonku.beritaid)
+
     app.route('/calonketua')
     .get(jsonku.calonketua);
+
+    app.route('/calonketua/:id')
+    .get(jsonku.calonketuaid);
     
     app.route('/informasidesa')
     .get(jsonku.informasidesa);
@@ -19,21 +25,41 @@ module.exports = function(app)  {
     app.route('/jenisumkm')
     .get(jsonku.jenisumkm);
 
+    app.route('/jenisumkm/:id')
+    .get(jsonku.jenisumkmid);
+
     app.route('/komentar')
     .get(jsonku.komentar);
+
+    app.route('/komentar/:id')
+    .get(jsonku.komentarid);
 
     app.route('/pemilihanketua')
     .get(jsonku.pemilihanketua);
 
+    app.route('/pemilihanketua/:id')
+    .get(jsonku.pemilihanketuaid);
+
     app.route('/pengaduanmasyarakat')
     .get(jsonku.pengaduanmasyarakat);
+
+    app.route('/pengaduanmasyarakat/:id')
+    .get(jsonku.pengaduanmasyarakatid);
 
     app.route('/pengurusdesaanggota')
     .get(jsonku.pengurusdesaanggota);
 
+    app.route('/pengurusdesaanggota/:id')
+    .get(jsonku.pengurusdesaanggotaid);
+
     app.route('/umkm')
     .get(jsonku.umkm);
+    app.route('/umkm/:id')
+    .get(jsonku.umkmid);
 
     app.route('/warga')
     .get(jsonku.warga);
+
+    app.route('/warga/:id')
+    .get(jsonku.wargaid);
 }
